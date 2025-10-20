17b8a3e4203cbaba3cd0447908b3e948b9d4f0e4
corneliusreed1910 <22073@virtual.utsc.edu.mx>
2025-10-19 21:57:03 -0600

    third commit

---
+++ second_Partial/ACT2_04.cpp
@@ -0,0 +1,22 @@
+//  Eduardo Peinado
+//  Student number: 22073
+//  Subject: Application for Internet of Things
+//  ACT2_04: ///////////////////////
+//  Fifth term
+//  TIADSM (Tecnologías de la Información Área Desarrollo Software Multiplataforma)
+//  Profe. Rocío del Carmen Martínez Luna
+//  Universidad Tecnológica Santa Catarina
+//  DATE  DATE  DATE
+//  Santa Catarina, Nuevo León, México
+
+#include <Arduino.h>
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+    
+}
