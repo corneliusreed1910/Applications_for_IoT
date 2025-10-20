c2f59c1f9ae8dfff9b284627994b86d1d063cad8
corneliusreed1910 <22073@virtual.utsc.edu.mx>
2025-10-19 22:03:57 -0600

    fourth commit

--- second_Partial/ACT2_04.cpp
+++ second_Partial/ACT2_04.cpp
@@ -1,7 +1,7 @@
 //  Eduardo Peinado
 //  Student number: 22073
 //  Subject: Application for Internet of Things
-//  ACT2_04: ///////////////////////
+//  ACT2_04: HC sensor with LEDs and buzzer
 //  Fifth term
 //  TIADSM (Tecnologías de la Información Área Desarrollo Software Multiplataforma)
 //  Profe. Rocío del Carmen Martínez Luna
@@ -11,9 +11,21 @@
 
 #include <Arduino.h>
 
+const int reds = 2;
+const int yellows = 3;
+const int greens = 4;
+const int echo = 5;
+const int trigger = 6;
+const int buzzer = 7;
+
 void setup()
 {
-    
+    pinMode(reds, OUTPUT);
+    pinMode(yellows, OUTPUT);
+    pinMode(greens, OUTPUT);
+    pinMode(trigger, OUTPUT); //I don't know which one is the INPUT at the moment
+    pinMode(echo, OUTPUT); //I don't know which one is the INPUT at the moment
+    pinMode(buzzer, OUTPUT);
 }
 
 void loop()
