d57f01dfc152a763e07bc5fda09aed86d8077ad2
corneliusreed1910 <22073@virtual.utsc.edu.mx>
2025-10-19 21:49:28 -0600

    second commit

--- first_Partial/ACT1_01.cpp
+++ first_Partial/ACT1_01.cpp
@@ -1,3 +1,14 @@
+//  Eduardo Peinado
+//  Student number: 22073
+//  Subject: Application for Internet of Things
+//  ACT1_01: turning a LED on
+//  Fifth term
+//  TIADSM (Tecnologías de la Información Área Desarrollo Software Multiplataforma)
+//  Profe. Rocío del Carmen Martínez Luna
+//  Universidad Tecnológica Santa Catarina
+//  DATE  DATE  DATE
+//  Santa Catarina, Nuevo León, México
+
 #include <Arduino.h>
 
 const int led = 2;
@@ -10,7 +21,4 @@ void setup()
 void loop()
 {
     digitalWrite(led, HIGH);
-    delay(1000);
-    digitalWrite(led, LOW);
-    delay(1000);
 }
